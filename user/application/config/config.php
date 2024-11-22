6b691a439b6c1c810d24ec53baa3c9c08e0a4826
Damar Adi N <damaradii.nugroho@gmail.com>
2024-11-22 16:21:26 +0700

    membenarkan error

--- user/application/config/config.php
+++ user/application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/BersihinAja/';
-$config['pilih'] = 'http://localhost/BersihinAja/pilih';
+$config['base_url'] = 'http://localhost/BersihinAja/user';
+$config['pilih'] = 'http://localhost/BersihinAja/user/pilih';
 
 
 /*
