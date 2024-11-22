66049cd5d19f8203ebecb5221788ca4fd43dc05f
samlehoy <muttaqien0111@gmail.com>
2024-11-22 12:52:00 +0700

    System dynamic

--- system/core/Loader.php
+++ system/core/Loader.php
@@ -36,6 +36,7 @@
  * @since	Version 1.0.0
  * @filesource
  */
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
@@ -49,6 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @author		EllisLab Dev Team
  * @link		https://codeigniter.com/userguide3/libraries/loader.html
  */
+#[\AllowDynamicProperties]
 class CI_Loader {
 
 	// All these are set automatically. Don't mess with them.
