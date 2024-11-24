1fc548d14832d805b5a762c3f1ca844080c0ede4
Native Muttaqien <muttaqien0111@gmail.com>
2024-11-25 00:07:20 +0700

    Service Detail Page

--- user/application/controllers/Service_Detail.php
+++ user/application/controllers/Service_Detail.php
@@ -1,10 +1,10 @@
 <?php 
-class Services extends CI_Controller {
+class Service_Detail extends CI_Controller {
 
 	function index() {
 
 		$this->load->view('header');
-        $this->load->view('services');	
+        $this->load->view('service_detail');	
 		$this->load->view('footer');
 		$this->load->helper('url');
 
