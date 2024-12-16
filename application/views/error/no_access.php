c2d8754f651a448f9087cd707d569d7d13ea5407
Native Muttaqien <muttaqien0111@gmail.com>
2024-12-16 23:26:07 +0700

    check

--- application/views/error/no_access.php
+++
@@ -1,7 +0,0 @@
-<div class="container mt-5">
-    <div class="alert alert-danger">
-        <h4>Akses Ditolak</h4>
-        <p><?= $message; ?></p>
-        <a href="<?php echo base_url('auth/logout'); ?>" class="btn btn-primary">Log out</a>
-    </div>
-</div>
