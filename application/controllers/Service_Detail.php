ac8aa31d5ddebb2e1d35ad105b6467bb3f088f8e
Native Muttaqien <muttaqien0111@gmail.com>
2025-06-20 20:56:45 +0700

    Update Service_Detail.php

--- application/controllers/Service_Detail.php
+++ application/controllers/Service_Detail.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
-class Service_Detail extends CI_Controller
+class Service_detail extends CI_Controller
 {
     public function __construct()
     {
